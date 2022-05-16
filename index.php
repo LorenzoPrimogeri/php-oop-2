58831ecff2158c5440c068a5375bc66ed281f0d3
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-05-16 15:47:46 +0200

    first class added

--- index.php
+++ index.php
@@ -12,10 +12,72 @@ BONUS:
 Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili
  solo in un periodo particolare (es. da maggio ad agosto).
 */
+class e_commerce
+{
+    public $nome;
+    function __construct($_name)
+    {
+        $this->nome = $_name;
+    }
+}
+
+class prodotto extends e_commerce
+{
+    public $settore;
+    public $nomeProdotto;
+    public $periodoDiDisponibilita;
+    public $prezzo;
+    function __construct($_settore, $_nomeProdotto, $_prezzo)
+    {
+        $this->nome = $_settore;
+        $this->nomeProdotto = $_nomeProdotto;
+        $this->prezzo = $_prezzo;
+    }
+}
+class pagamenti extends e_commerce
+{
+    public $nomeCarta;
+    public $banca;
+    public $iban;
+}
+class utente extends e_commerce
+{
+    public $nomeUtente;
+    public $cognomeUtente;
+    public $codiceFiscale;
+    public $registrato; //tipo booleano, true o false
+    public $carrello; //totale dei prodotti che vuole acquistare
+    public $prodottoDaAcquistare;
+    public $prodottiAcquistati;
+    function __construct($_name, $_cognome, $_codiceFiscale)
+    {
+        $this->nome = $_name;
+        $this->cognome = $_cognome;
+        $this->codiceFiscale = $_codiceFiscale;
+    }
+    function set($_carrello)
+    {
+        $this->carrello = $_carrello;
+    }
+    function sconto()
+    {
+        if ($this->registrato == true) {
+            $prezzoFinale = $carrello * 20 / 100;
+            return $carrello = -$prezzoFinale;
+        }
+    }
+}
+class cartaDiCredito extends utente
+{
+    public $nomeTitolare;
+    public $scadenza;
+    public $numeroCarta;
+    public $iban;
+    public $cvv;
+    public $scaduta;
 
 
-
-
+}
 ?>
 
 <h1>OOP_2</h1>
